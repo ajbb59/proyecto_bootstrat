22c32c594a272d356b7e7e26d5e8c8a2c007209b
ajbb59 <48368668+ajbb59@users.noreply.github.com>
2022-12-06 10:37:56 -0500

    Add files via upload
    Signed-off-by: ajbb59 <48368668+ajbb59@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<?php
+header("Refresh:7");
+//echo date('H:i:s Y-m-d');
+?>
+<html>
+<head>
+	<title>Graficos con plotly</title>
+	<link rel="stylesheet" type="text/css" href="librerias/bootstrap/css/bootstrap.css">
+	<script src="librerias/jquery-3.3.1.min.js"></script>
+	<script src="librerias/plotly-latest.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="panel panel-primary">
+					<div class="panel panel-heading">
+						Gráficos Comparativo de Ventas
+					</div>
+					<div class="panel panel-body">
+						<div class="row">
+							<div class="col-sm-6">
+								<div id="cargaLineal"></div>
+							</div>
+							<div class="col-sm-6">
+								<div id="cargaBarras"></div>
+							</div>
+							<div class="col-sm-4">
+								<div id="cargaBarras2"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#cargaLineal').load('lineal.php');
+		$('#cargaBarras').load('barras.php');
+//		$('#cargaBarras2').load('barras0.php');
+	});
+</script>
